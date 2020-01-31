5a335b37815e5475185938060baaa53b5de0c99f
Juan Leni <lenijuan@gmail.com>
2020-01-31 12:25:04 +0100

    improve unit tests

--- tests/basic.ispec.js
+++ tests/basic.ispec.js
@@ -2,8 +2,8 @@ import FilecoinApp from "index.js";
 import TransportNodeHid from "@ledgerhq/hw-transport-node-hid";
 import { expect, test } from "jest";
 import secp256k1 from "secp256k1/elliptic";
-import blake2 from "blake2";
-import { ERROR_CODE } from "../src/common";
+import Message from "@openworklabs/filecoin-message";
+import { ERROR_CODE, getDigest, PKLEN } from "../src/common";
 
 test("get version", async () => {
   const transport = await TransportNodeHid.create();
@@ -44,14 +44,14 @@ test("getAddressAndPubKey", async () => {
     expect(resp).toHaveProperty("addrString");
     expect(resp).toHaveProperty("compressed_pk");
 
-    expect(resp.compressed_pk.length).toEqual(33);
+    expect(resp.compressed_pk.length).toEqual(PKLEN);
     expect(resp.compressed_pk.toString("hex")).toEqual(
-      "0325d0dbeedb2053e690a58e9456363158836b1361f30dba0332f440558fa803d0",
+      "04240ecf6ec722b701f051aaaffde7455a56e433139e4c0ff2ad7c8675e2cce104a8027ba13e5bc640ec9932cce184f33a789bb9c32f41e34328118b7862fc9ca2",
     );
 
-    expect(resp.addrByte.toString("hex")).toEqual("0171d1b27cf522114294029c5a42351f716cbb6c65");
+    expect(resp.addrByte.toString("hex")).toEqual("0175a6b113220c2f71c4db420753aab2cef5edb6a8");
 
-    expect(resp.addrString).toEqual("f1ohi3e7hveiiuffactrneeni7ofwlw3dfaglieba");
+    expect(resp.addrString).toEqual("f1owtlcezcbqxxdrg3iidvhkvsz3263nvijwpumui");
   } finally {
     transport.close();
   }
@@ -77,14 +77,46 @@ test("showAddressAndPubKey", async () => {
     expect(resp).toHaveProperty("addrString");
     expect(resp).toHaveProperty("compressed_pk");
 
-    expect(resp.compressed_pk.length).toEqual(33);
+    expect(resp.compressed_pk.length).toEqual(PKLEN);
     expect(resp.compressed_pk.toString("hex")).toEqual(
-      "03b481eeff158ba0044fa075b2a53cb34de11193699e0fd0ee8abb10fa2acd9bc3",
+      "04fc016f3d88dc7070cdd95b5754d32fd5290f850b7c2208fca0f715d35861de1841d9a342a487692a63810a6c906b443a18aa804d9d508d69facc5b06789a01b4",
     );
 
-    expect(resp.addrByte.toString("hex")).toEqual("014e14ae1814ac4c91e97a77dbaeabe27a4dcba54c");
+    expect(resp.addrByte.toString("hex")).toEqual("018bab69a28eeb4525bd8f49679a740a9582691906");
 
-    expect(resp.addrString).toEqual("f1jykk4gauvrgjd2l2o7n25k7cpjg4xjkmxpoklzy");
+    expect(resp.addrString).toEqual("f1rovwtiuo5ncslpmpjftzu5akswbgsgighjazxoi");
+  } finally {
+    transport.close();
+  }
+});
+
+test("getAddressAndPubKeyTestnet", async () => {
+  jest.setTimeout(60000);
+  const transport = await TransportNodeHid.create();
+  try {
+    const app = new FilecoinApp(transport);
+
+    const path = [44, 1, 0, 0, 0];
+    const resp = await app.getAddressAndPubKey(path);
+
+    // eslint-disable-next-line no-console
+    console.log(resp);
+
+    expect(resp.return_code).toEqual(0x9000);
+    expect(resp.error_message).toEqual("No errors");
+
+    expect(resp).toHaveProperty("addrByte");
+    expect(resp).toHaveProperty("addrString");
+    expect(resp).toHaveProperty("compressed_pk");
+
+    expect(resp.compressed_pk.length).toEqual(PKLEN);
+    expect(resp.compressed_pk.toString("hex")).toEqual(
+      "0466f2bdb19e90fd7c29e4bf63612eb98515e5163c97888042364ba777d818e88b765c649056ba4a62292ae4e2ccdabd71b845d8fa0991c140f664d2978ac0972a",
+    );
+
+    expect(resp.addrByte.toString("hex")).toEqual("01dfe49184d46adc8f89d44638beb45f78fcad2590");
+
+    expect(resp.addrString).toEqual("t137sjdbgunloi7couiy4l5nc7pd6k2jmq32vizpy");
   } finally {
     transport.close();
   }
@@ -160,18 +192,7 @@ test("sign_and_verify", async () => {
     expect(responseSign.error_message).toEqual("No errors");
 
     // Calculate message digest
-    // digest = blake2-256( prefix + blake2b-256(tx) )
-    let hasher;
-
-    hasher = blake2.createHash("blake2b", { digestLength: 32 });
-    hasher.update(message);
-    const tmp = hasher.digest();
-
-    hasher = blake2.createHash("blake2b", { digestLength: 32 });
-    const prefix = Buffer.from([0x01, 0x71, 0xa0, 0xe4, 0x02, 0x20]);
-    hasher.update(prefix);
-    hasher.update(tmp);
-    const msgDigest = hasher.digest();
+    const msgDigest = getDigest(message);
 
     // Check signature is valid
     const signatureDER = responseSign.signature_der;
@@ -185,6 +206,62 @@ test("sign_and_verify", async () => {
 
     const signatureOk = secp256k1.ecdsaVerify(signature, msgDigest, responsePk.compressed_pk);
     expect(signatureOk).toEqual(true);
+  } finally {
+    transport.close();
+  }
+});
+
+test("sign_and_verify_testnet", async () => {
+  jest.setTimeout(60000);
+  const transport = await TransportNodeHid.create();
+  try {
+    const app = new FilecoinApp(transport);
+
+    // Derivation path. First 3 items are automatically hardened!
+    const path = [44, 1, 0, 0, 0];
+
+    const messageContent = new Message({
+      from: "t137sjdbgunloi7couiy4l5nc7pd6k2jmq32vizpy",
+      to: "t1t5gdjfb6jojpivbl5uek6vf6svlct7dph5q2jwa",
+      value: "1000",
+      method: 0,
+      gasPrice: "1",
+      gasLimit: "1000",
+      nonce: 0,
+    });
+
+    const message = await messageContent.serialize();
+
+    const responsePk = await app.getAddressAndPubKey(path);
+    const responseSign = await app.sign(path, message);
+
+    expect(responsePk.return_code).toEqual(ERROR_CODE.NoError);
+    expect(responsePk.error_message).toEqual("No errors");
+    expect(responseSign.return_code).toEqual(ERROR_CODE.NoError);
+    expect(responseSign.error_message).toEqual("No errors");
+
+    // Calculate message digest
+    const msgDigest = getDigest(message);
+    console.log(`Digest: ${msgDigest.toString("hex")}`);
+
+    // Check signature is valid
+    const signatureDER = responseSign.signature_der;
+    const signature = secp256k1.signatureImport(signatureDER);
+    console.log(`DER   : ${responseSign.signature_der.toString("hex")}`);
+
+    // Check compact signatures
+    const sigBuf = Buffer.from(signature);
+    const sigCompBuf = Buffer.from(responseSign.signature_compact.slice(0, 64));
+    console.log(`compact   : ${Buffer.from(responseSign.signature_compact).toString("hex")}`);
+
+    expect(sigBuf).toEqual(sigCompBuf);
+
+    const signatureOk = secp256k1.ecdsaVerify(signature, msgDigest, responsePk.compressed_pk);
+    expect(signatureOk).toEqual(true);
+
+    console.log(`compact   : ${responseSign.signature_compact.toString("base64")}`);
+
+    // Prepare curl command
   } finally {
     transport.close();
   }
